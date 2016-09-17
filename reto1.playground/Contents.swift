ce76026fbd8ba4322a5e4963ef52739ed7ad0934
Sergio Antonio Pohlmann <sergio.pohlmann@gmail.com>
2016-09-17 16:49:06 -0400

    Primer_Ejecicio_Coursera updated

---
+++ reto1.playground/Contents.swift
@@ -0,0 +1,73 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+// VERISION UNO
+print( "VERSION UNO ")
+for numero in 0...100 {
+    
+    
+    if numero > 0 {
+    
+        // Verifica numero divisible entre 5
+    
+        if (numero % 5) == 0 {
+            print( "#\(numero) - Bingo!!!")
+        }
+    
+        // Verifica numero par / impar
+        if (numero % 2) == 0 {
+            print( "#\(numero) - par!!!")
+        }
+        else {
+            print( "#\(numero) - impar!!!" )
+        }
+    
+        // Verifica si está en el rango 30 al 40
+    
+        if numero >= 30 && numero <= 40  {
+            print( "#\(numero) - Viva Swift!!!")
+        }
+        
+    }
+    else {
+        print( "#\(numero)")
+    }
+}
+
+
+
+// VERISION DOS
+
+var label = ""
+print( "\n\nVERSION DOS")
+for numero in 0...100 {
+    
+    label = " - "
+        
+    // Verifica numero par / impar
+    if (numero % 2) == 0 && numero > 0 {
+        label += "  Par!!!  "
+    }
+    else if numero > 0 {
+        label += "  Impar!!!"
+    }
+    
+    // Verifica numero divisible entre 5
+        
+    if (numero % 5) == 0 && numero > 0 {
+        label += "  Bingo!!!"
+    }
+    else {
+        label += "          "
+    }
+    
+    
+    // Verifica si está en el rango 30 al 40
+    if numero >= 30 && numero <= 40  {
+        label += "  Viva Swift!!!"
+    }
+
+    print( "# \(numero) \(label)")
+}
