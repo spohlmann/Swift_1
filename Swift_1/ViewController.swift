0e2ce47848f20884e17e31e1ec9f86ca8b1e5c5d
Sergio Antonio Pohlmann <sergio.pohlmann@gmail.com>
2016-09-17 16:00:05 -0400

    Initial Commit

---
+++ Swift_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_1
+//
+//  Created by Sergio Antonio Pohlmann on 9/17/16.
+//  Copyright © 2016 Sergio Antonio Pohlmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
